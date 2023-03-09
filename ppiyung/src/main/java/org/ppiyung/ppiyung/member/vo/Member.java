3df071f10bfe5867ce17cc002a34bd3ed4565077
unknown <ohkim12345@gmail.com>
2023-03-09 14:30:20 +0900

    03/09 14:30 memberVO 및 memberMapper.xml 수정

--- ppiyung/src/main/java/org/ppiyung/ppiyung/member/vo/Member.java
+++ ppiyung/src/main/java/org/ppiyung/ppiyung/member/vo/Member.java
@@ -14,18 +14,19 @@ public class Member {
 	private String member_pw;
 	private String member_name;
 	private Date member_birth;
-	private boolean member_gender;
+	private char member_gender;
 	private String member_phone;
 	private String member_addr;
 	private double member_coord_x;
 	private double member_coord_y;
 	private String member_nickname;
 	private String member_email;
-	private int member_type;
+	private char member_type;
 	private String member_reg_num;
 	private String member_info;
 	private boolean member_active;
 	private Date member_created_at;
 	private int work_area_id;
 	private String member_img;
+	private boolean member_verified;
 }

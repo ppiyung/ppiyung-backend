e5995bd247492e6e843f65e1dc9d096d7fec1352
gkswotjd45 <gkswotjd45@naver.com>
2023-03-10 09:09:46 +0900

    23.03.10 09:00 게시판 전체 게시글 쿼리 완

--- ppiyung/src/main/java/org/ppiyung/ppiyung/board/vo/Board.java
+++ ppiyung/src/main/java/org/ppiyung/ppiyung/board/vo/Board.java
@@ -11,11 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Board {
-	private int article_id; // 게시글 아읻
+	private int article_id; // 게시글 아이디
+	private String member_nickname; //회원 닉네임
 	private String article_title; // 게시글 제목
 	private String article_content; // 게시글 내용
 	private String member_id; // 회원 아이디
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.S")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date article_created_at; // 게시글 작성일시
 	private String comment_cnt; // 댓글 수
 	private int like_cnt;// 좋아요 개수

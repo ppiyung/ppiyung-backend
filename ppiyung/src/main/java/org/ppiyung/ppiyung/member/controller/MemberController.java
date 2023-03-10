5284848aa7e6dfdb509e6fba196c142198340d81
unknown <ohkim12345@gmail.com>
2023-03-10 14:37:18 +0900

    03/10 회원 정보 수정중

--- ppiyung/src/main/java/org/ppiyung/ppiyung/member/controller/MemberController.java
+++ ppiyung/src/main/java/org/ppiyung/ppiyung/member/controller/MemberController.java
@@ -3,10 +3,13 @@ package org.ppiyung.ppiyung.member.controller;
 
 import java.nio.charset.Charset;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.tools.picocli.CommandLine.Parameters;
 import org.ppiyung.ppiyung.common.entity.BasicResponseEntity;
 import org.ppiyung.ppiyung.member.service.MemberService;
 import org.ppiyung.ppiyung.member.vo.Member;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,7 +33,7 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
-//회원가입
+	//회원가입
 	@PostMapping(value="signin")
 	public ResponseEntity<BasicResponseEntity<Object>>
 			signinHandler(@RequestBody Member reqSigninInfo ){
@@ -53,13 +57,20 @@ public class MemberController {
 	
 				return new ResponseEntity<BasicResponseEntity<Object>>(respBody, headers, respCode);
 	}
+	  //개별회원조회
+	 @GetMapping (value = "")
+	 public void getMember(HttpSession session){
+//		HttpSession session = request.getSession(true);
 	
+		Member currentMember = (Member)session.getAttribute("currentMember");
+		
+		 
+	 }
 	
-	
-//회원수정
+	//회원수정
 	@PutMapping(value="")
 	public ResponseEntity<BasicResponseEntity<Object>>
-			req(@RequestBody Member reqUpdateInfo ){
+			modifyMember(@RequestBody Member reqUpdateInfo ){
 		
 		
 		boolean result = service.modifyMember(reqUpdateInfo);
@@ -72,6 +83,7 @@ public class MemberController {
 		if(result == true) {
 			log.debug("회원정보수정 성공");
 			respBody = new BasicResponseEntity<Object>(true, "회원정보수정에 성공하였습니다.", result);
+			//셋어트리뷰트
 			respCode = HttpServletResponse.SC_OK;
 		}else {
 			log.debug("회원정보수정 실패");
@@ -82,8 +94,9 @@ public class MemberController {
 	}
 	
 	
-	
-	
+// 회원정보를 수정하면 세션을 날리고, // 새 세션을 부여해서 그 정보를 넣어햐 함
+	// 회원 조회는 항상 새로운 세션을 넣어야 함 
+	 
 	
 
 }
--- ppiyung/src/main/java/org/ppiyung/ppiyung/recruit/controller/RecruitController.java
+++ ppiyung/src/main/java/org/ppiyung/ppiyung/recruit/controller/RecruitController.java
@@ -99,5 +99,8 @@ public class RecruitController {
 		
 
 	}
+	
+	
+
 }
 

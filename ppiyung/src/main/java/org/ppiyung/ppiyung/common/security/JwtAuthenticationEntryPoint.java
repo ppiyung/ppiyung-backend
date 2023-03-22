23f236638aef103a1e71732ca3acc39faaed0994
0tak2 <0tak2.code@gmail.com>
2023-03-22 12:27:04 +0900

    3/22 12:26 jwtAuthenticationEntryPoint에서 allowOrigin 설정 security-context.xml로 외부화

--- ppiyung/src/main/java/org/ppiyung/ppiyung/common/security/JwtAuthenticationEntryPoint.java
+++ ppiyung/src/main/java/org/ppiyung/ppiyung/common/security/JwtAuthenticationEntryPoint.java
@@ -6,16 +6,23 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-	@Value("${auth.allowOrigin:http://localhost:9090}")
+
 	private String allowOrigin;
 	
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    	System.out.println(allowOrigin);
+    	
 		response.setContentType("application/json; charset=UTF-8"); 
 		response.setHeader("Access-Control-Allow-Origin", allowOrigin);
 		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
